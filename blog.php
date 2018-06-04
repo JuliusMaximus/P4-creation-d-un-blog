e882531c3b6359efee9448182ce0e0f14c05a677
JuliusMaximus <contact@weckjulien.fr>
2018-06-04 18:27:58 +0200

    ajout des fichiers: lecture_billet.php, lecture_billet.scss / modification blog.php / création de la base de donnée avec ajout des tables billets et commentaires

--- blog.php
+++ blog.php
@@ -34,6 +34,63 @@
 
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duo Reges: constructio interrete. Nam ista vestra: Si gravis, brevis; Propter nos enim illam, non propter eam nosmet ipsos diligimus. Gracchum patrem non beatiorem fuisse quam fillum, cum alter stabilire rem publicam studuerit, alter evertere. Hoc loco tenere se Triarius non potuit. Quis est tam dissimile homini. </p>
 
+		<?php
+
+		// Connexion à la base de données
+
+		try
+
+		{
+
+		    $bdd = new PDO('mysql:host=localhost;dbname=bdd_blog;charset=utf8', 'root', '');
+
+		}
+
+		catch(Exception $e)
+
+		{
+
+	        die('Erreur : '.$e->getMessage());
+
+		}
+
+		// On récupère les 5 derniers billets
+
+		$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
+
+		while ($donnees = $req->fetch())
+
+		{
+
+		?>
+
+		<div class="news">
+		    <h3>
+		        <?php echo htmlspecialchars($donnees['titre']); ?>
+
+		        <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+
+		    </h3>
+		    <p>
+
+		    <?php
+		    // On affiche le contenu du billet
+		    echo nl2br(htmlspecialchars($donnees['contenu']));
+		    ?>
+		    	<br />
+		    	<em><a href="lecture_billet.php?billets=<?php echo $donnees['id']; ?>">Commentaires</a></em>
+		    </p>
+		</div>
+
+		<?php
+
+		} // Fin de la boucle des billets
+
+		$req->closeCursor();
+
+		?>
+
 		<div class="media">
 		  <img class="align-self-center mr-3" src="img/aurora-small.jpg" alt="Generic placeholder image">
 		  <div class="media-body">
