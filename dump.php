da112d69785cf6ec182869f8eee11f5ce0be5621
JuliusMaximus <contact@weckjulien.fr>
2018-09-01 17:49:36 +0200

    suppression dump.php

---
+++ dump.php
@@ -0,0 +1,12 @@
+<?php
+array(1) { 
+	[0]=> array(12) { 
+		["id"]=> string(2) "24" [0]=> string(2) "24" ["id_project"]=> string(2) "10" [1]=> string(2) "10" ["author"]=> string(6) "pierre" [2]=> string(6) "pierre" ["comment"]=> string(270) "
+
+Le message a été supprimé car il ne respècte pas les charte de bonne conduite du blog ! Pour toutes réclamation merci de nous contacter via le formulaire.
+" [3]=> string(270) "
+
+Le message a été supprimé car il ne respècte pas les charte de bonne conduite du blog ! Pour toutes réclamation merci de nous contacter via le formulaire.
+" ["created_at"]=> string(19) "2018-08-18 15:19:14" [4]=> string(19) "2018-08-18 15:19:14" ["reported"]=> string(1) "0" [5]=> string(1) "0" 
+} 
+} 
